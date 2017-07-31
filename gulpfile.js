098b7fc35692a6a9489019457b9650af6a3c81a5
eczn <1975845422@qq.com>
2017-08-01 01:47:50 +0800

    读取模版文件

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,32 @@
 // gulpfile.js
 var gulp = require('gulp'); 
 var connect = require('gulp-connect'); 
+var rename = require("gulp-rename");
+var header = require('gulp-header');
+var footer = require('gulp-footer');
+var path = require('path'); 
 
 var CONFIG = {
-	src: './src/**/*', 
-	dest: './dest/',
-	toWatch: './dest/**/*'
+	src:  path.join(__dirname, './src/**/*'), 
+	dest:  path.join(__dirname, './dest/'),
+	toWatch:  ['./dest/**/*', '!./dest/tpl/*'].map(d => path.join(__dirname, d)), 
+	tplSrc:  path.join(__dirname, './src/tpl/*.html'),
+	tplDest: path.join(__dirname, './dest/tpl/')
 }
 
 // localhost:8080 
-connect.server({
-	root: CONFIG.dest,
-	port: 8080,
-	livereload: true
-});
+gulp.task('start', function(){
+	connect.server({
+		root: CONFIG.dest,
+		port: 8080,
+		livereload: true
+	});
+
+	gulp.watch(CONFIG.src, ['copy', 'reload']); 
+})
 
 
-gulp.watch(CONFIG.src, ['copy', 'reload']); 
+
 
 gulp.task('reload', function(){
 	return gulp.src(CONFIG.toWatch)
@@ -28,7 +38,37 @@ gulp.task('copy', function(){
 		.pipe(gulp.dest(CONFIG.dest)); 
 }); 
 
+// Header 
+var headerTpl = `
+var <%= naming(filename) + 'Render' %> = (function(tpl){
+	return tpl.fromStr(
+	 \`
+`;
+
+// Footer 
+var footerTpl = `
+	\`); 
+})(window.tpl); 
+`; 
+
+gulp.task('tpl', function(){
+	return gulp.src(CONFIG.tplSrc)
+		.pipe(rename(function (path) {
+			path.basename += "Tpl";
+			path.extname = ".js";
+		}))
+		.pipe(header(headerTpl, {
+			naming: name => path.parse(name).name
+		}))
+		.pipe(footer(footerTpl, {
+			naming: name => path.parse(name).name
+		}))
+		.pipe(gulp.dest(CONFIG.tplDest)); 
+}); 
+
+
+
 
 // 别名
-gulp.task('serve', ['copy']); 
+gulp.task('serve', ['start', 'copy', 'tpl']); 
 
--- src/js/main.js
+++ src/js/main.js
@@ -1,24 +1,5 @@
 // main.js
-function drawDC(data){
-	var dailyCSSList = `
-		{{ get (item, idx) >>>> list2 }}
-			<span> {{ item }} </span>
-		{{ teg }}
-
-		<ul>
-			{{ get (item, idx) >>>> list }}
-			<li> {{ item }} </li>
-			{{ teg }}
-		</ul>
-	`; 
-
-	
-
-	var DCRender = tpl.fromStr(dailyCSSList); 
-
-	var result = DCRender(data); 
-
-	$('.container').html(result)
-}
-
+var res = testTplRender({
+	list: ['A', 'B', 'C']
+}); 
 
