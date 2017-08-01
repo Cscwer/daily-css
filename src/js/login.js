0b09a40ebc543da862b40cbef83279866256d9c5
yitianZheng <895984255@qq.com>
2017-08-01 16:26:47 +0800

    latest

--- src/js/login.js
+++ src/js/login.js
@@ -1,1 +1,36 @@
 // login.js
+// 
+$(function(){
+	$('#password').togglePassword({
+		el: '#togglePassword'
+	});
+
+});
+
+function hasClass(obj, cls) {  
+    return obj.className.match(new RegExp('(\\s|^)' + cls + '(\\s|$)'));  
+}  
+  
+function addClass(obj, cls) {  
+    if (!this.hasClass(obj, cls)) obj.className += " " + cls;  
+}  
+  
+function removeClass(obj, cls) {  
+    if (hasClass(obj, cls)) {  
+        var reg = new RegExp('(\\s|^)' + cls + '(\\s|$)');  
+        obj.className = obj.className.replace(reg, ' ');  
+    }  
+}  
+  
+function toggleClass(obj,cls){  
+    if(hasClass(obj,cls)){  
+        removeClass(obj, cls);  
+    }else{  
+        addClass(obj, cls);  
+    }  
+}  
+  
+function toggleClassTest(){  
+    var obj = document. getElementById('togglePassword');  
+    toggleClass(obj,"icon-yanjing");  
+}  
