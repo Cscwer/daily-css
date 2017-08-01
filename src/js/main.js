086722d9b0c9fc9ac4bead43e44d2522cec28ddd
IFE <971340448@qq.com>
2017-08-01 12:15:26 +0800

    daily commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,80 @@
 // main.js
-function drawDC(data){
+
+
+
+// function drawDC(data){
 
 	
 	
-	var dailyCSSList = `
-		{{ get (item, idx) >>>> list2 }}
-			<span> {{ item }} </span>
-		{{ teg }}
+// 	var dailyCSSList = `
+// 		{{ get (item, idx) >>>> list2 }}
+// 			<span> {{ item }} </span>
+// 		{{ teg }}
 
-		<ul>
-			{{ get (item, idx) >>>> list }}
-			<li> {{ item }} </li>
-			{{ teg }}
-		</ul>
-	`; 
+// 		<ul>
+// 			{{ get (item, idx) >>>> list }}
+// 			<li> {{ item }} </li>
+// 			{{ teg }}
+// 		</ul>
+// 	`; 
 
 	
 
-	var DCRender = tpl.fromStr(dailyCSSList); 
+// 	var DCRender = tpl.fromStr(dailyCSSList); 
 
-	var result = DCRender(data); 
+// 	var result = DCRender(data); 
 
-	$('.container').html(result);
+// 	$('.container').html(result);
 
-}
+// }
+
+
+$(document).ready(function(){
+
+// 渲染daily-css
+	function dailyCss(data){
+
+		var dailyTem = `
+			<p> {{ a }} </p>
+		`;
+
+		var dailyRender = tpl.fromStr(dailyTem);
+
+		var result = dailyRender(data);
+
+		$('.daily-css').html(result);
+	}
+
+
+	test = {
+		a: '那Line-height行高属性。一般我们设置<br />设置line-height行高属性即可实现让ght行高属性即可实现让ght行高属性即自动换行文字排版均匀间隔'
+	};
+
+	dailyCss(test);
+
+	// var aa = $('.main-container textarea').change(function(){
+	// 	console.log(this.value.length);
+	// });
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
