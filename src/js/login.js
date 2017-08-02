08d192b940a2993b020a38cd58c3117a432025cc
yitianZheng <895984255@qq.com>
2017-08-02 17:25:58 +0800

    latest

--- src/js/login.js
+++ src/js/login.js
@@ -1,5 +1,5 @@
 // login.js
-// 
+
 $(function(){
 	$('#password').togglePassword({
 		el: '#togglePassword'
@@ -34,3 +34,59 @@ function toggleClassTest(){
     var obj = document. getElementById('togglePassword');  
     toggleClass(obj,"icon-yanjing");  
 }  
+
+function getUser() {
+	var name = $(".login-user").val();
+	var pwd = $(".login-pass").val();	
+	if(name.length === 0) {
+		alert("请输入用户名！");
+		return false;
+	}
+	return {
+		username: name,
+		pwd: pwd
+	}
+}
+
+function login() {
+	var user = getUser();
+	if(!user) {
+		return;
+	}
+	// $.ajax({
+	// 	url: "http://192.168.0.112:3000/login/login",
+	// 	type: "POST",
+	// 	data: user,
+	// 	dataType: "json",
+	// 	success: function(res) {
+	// 		console.log(res);
+	// 		if(res.code === 403) {
+	// 			$(".login-prove").css("display","block");
+	// 			$(".login-user").val("");
+	// 			$(".login-pass").val("");
+	// 		}
+	// 	},
+	// 	error: function(xhr, err, type) {
+	// 		console.log(xhr);
+	// 		console.log(err);
+	// 		console.log(type);
+	// 	}
+	// })
+	http.login(
+		user,
+		function(res) {
+			if(res.code === 403) {
+				$(".login-prove").css("display","block");
+				$(".login-user").val("");
+				$(".login-pass").val("");
+			}
+		},
+		function(xhr, err, type) {
+			console.log(xhr);
+			console.log(err);
+			console.log(type);
+		}
+	)
+}
+
+$(".login-log").click(login);
