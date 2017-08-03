3c0f6ad54a5822ccdd64d2858575f0fccd115e7f
IFE <971340448@qq.com>
2017-08-04 00:05:36 +0800

    style complete

--- src/js/main.js
+++ src/js/main.js
@@ -57,7 +57,7 @@
 			{{ get (item, idx) >>>> list }}
 				<div class="favour-container">
 					<div class="dc-favour"> {{ item.theContent }} </div>
-					<div></div>
+					<div class="{{ addMask item.theContent }}"></div>
 					<div class="favour-delete"></div>
 					<div class="favour-detail"></div>
 				</div>
@@ -66,34 +66,17 @@
 		var favoursRender = tpl.fromStr(favourTem);
 
 		var result = favoursRender({
-			list: favorite
+			list: favorite,
+			// 判断收藏夹内单条dc的长度，大于42则加模糊
+			addMask: str => str.length > 42 ? 'favour-mask' : ''
 		});
 
 		$('.dc-favours').html(result);
 
-
-
-		// 判断收藏夹内单条dc的字数
-
-		var favour = $('.dc-favour');
-		// favour.each((idx) => {
-		// 	console.log(idx);
-		// 	console.log(fuck[idx].theContent.length);
-		// 	if (fuck[idx].theContent.length > 42) {
-		// 		$(this).next().addClass('favour-mask');
-		// 	}
-		// });
-
-		for (var i = 0; i < fuck.length; i++) {
-			if (fuck[i].theContent.length > 42) {
-				$(favour[i]).next().addClass('favour-mask');
-			}
-		}
-
 	}
 	var fuck = [
 		{
-			theContent: '哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦oooo哦',
+			theContent: '哦哦哦哦哦哦哦哦fs jk fsj哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦oooo哦',
 		}, 
 		{
 			theContent: '哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦哦'
@@ -124,47 +107,160 @@
 
 
 
+// 渲染详情页的评论
+function drawComment(comments){
+	var commentTem = `
+		{{ get (item, idx) >>>> list }}
+			<div class="{{ judge item.commentator username }}">
+				<div class="user-pic"></div>
+				<div class="user-comment"> {{ item.comment }} </div>
+			</div>
+		{{ teg }}
+	`
+	var commentRender = tpl.fromStr(commentTem);
+	var result = commentRender({
+		list: comments,
+		judge: (commentator,username) => commentator === username ? 'comment-self' : 'comment-others'
+	});
+	$('.comment-container').html(result);
+}
+
+
+var check = {
+    "code": 200,
+    "data": [
+        {
+            "_id": "5982955c00d6960df4180b73",
+            "id": "1f1e862e-7b29-46e5-834b-e6f3ec3d21bf",
+            "commentator": "321",
+            "comment": "荣耀大傻逼",
+            "date": "2017-08-03 11:15:40",
+            "author": "zxc111",
+            "status": "1"
+        },{
+            "_id": "5982951700d6960df4180b71",
+            "id": "1f1e862e-7b29-46e5-834b-e6f3ec3d21bf",
+            "commentator": "zxc111",
+            "comment": "我要吃鸡腿",
+            "date": "2017-08-03 11:14:31",
+            "author": "zxc111",
+            "status": "1"
+        },
+        {
+            "_id": "5982955500d6960df4180b72",
+            "id": "1f1e862e-7b29-46e5-834b-e6f3ec3d21bf",
+            "commentator": "1233",
+            "comment": "gtt",
+            "date": "2017-08-03 11:15:33",
+            "author": "zxc111",
+            "status": "1"
+        },
+        {
+            "_id": "5982951700d6960df4180b71",
+            "id": "1f1e862e-7b29-46e5-834b-e6f3ec3d21bf",
+            "commentator": "zxc111",
+            "comment": "啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊",
+            "date": "2017-08-03 11:14:31",
+            "author": "zxc111",
+            "status": "1"
+        },{
+            "_id": "5982951700d6960df4180b71",
+            "id": "1f1e862e-7b29-46e5-834b-e6f3ec3d21bf",
+            "commentator": "zxc11d1",
+            "comment": "大傻逼啊",
+            "date": "2017-08-03 11:14:31",
+            "author": "zxc11d1",
+            "status": "1"
+        },{
+            "_id": "5982951700d6960df4180b71",
+            "id": "1f1e862e-7b29-46e5-834b-e6f3ec3d21bf",
+            "commentator": "zxc111",
+            "comment": "哦",
+            "date": "2017-08-03 11:14:31",
+            "author": "zxc111",
+            "status": "1"
+        }
+    ],
+    "dailyCss": {
+        "_id": "5982941b00d6960df4180b6b",
+        "username": "zxc111",
+        "content": "现在是中午",
+        "date": "2017-08-03 11:10:19",
+        "id": "1f1e862e-7b29-46e5-834b-e6f3ec3d21bf"
+    },
+    "msg": "成功查看评论"
+};
+// var username = check.dailyCss.username;
+// tpl.push({
+// 	username: username
+// }); 
+// drawComment(check.data);
+// tpl.pop(); 
+
+
+
 $(document).ready(function(){
 
-// 渲染daily-css
-	function drawDC(data){
-
+// 渲染daily-css	
+	function drawDC(data,where,hasIcon){
 		var dailyTem = `
 			<p class="init-text"> {{ a }} </p>
+
+			{{ if hasIcon }}
 			<span class="favour-dc"></span>
 			<span class="detail-dc"></span>
+			{{ else }}
+
+			{{ fi }}
 		`;
 
 		var dailyRender = tpl.fromStr(dailyTem);
 
-		var result = dailyRender(data);
+		var copy = JSON.stringify(data); 
+		copy = JSON.parse(copy); 
+		copy.hasIcon = hasIcon; 
 
-		$('.daily-css').html(result);
+		var result = dailyRender(copy);
+
+		$(where).html(result);
 	}
 
 
 	test = {
-		a: '解好回复都受到世界各地是个电视剧世界各地是个电是个电视剧解不开高速公路上你了离开国欢可能你也好回复都受到世界各地是个电视剧解不开高速公路上你了离开国欢可能你也好回复都受到世界各地是个电视剧解不开高速公路上你了离开国欢可能你也好回复都受到世界各地是个电视剧',
+		a: '解好回复都受到世界各地是个电视剧世界各地是个电是个电视剧解不开高速公路上你了离开国欢可能你也好回复都受到世界各地是个电视剧解不开高速公路上你了离开国欢可能你也好回复都受到世界各地是个电视剧解不开高速公路上你了离开国欢可能你也好回复都受到世界各地是个电视剧到世界各地是个电视剧解不开高速公路上你了离开国欢可能你也好回复都受到世界各地是个电视剧',
 		b: 'fsfsfs'
 	};
 
-	drawDC(test);
+// true or false是判断是否加图标的参数
+	drawDC(test,$('.daily-css'),true);
 
+	drawDC(test,$('.show-dc'),false);
 
-
-
-
-
+// 整个主界面
+	var dcContainer = $('.main-container');
+// 主界面的dc
 	var dc = $('.daily-css');
+// 主界面的收藏夹
 	var dcFavour = $('.dc-favours');
+// 主界面的书签
+	var nav = $('.nav');
+
 	var textarea = $('.main-container textarea');
 	var submitBtn = $('.submit-btn');
 	var dcTxt = $('.daily-css p');
+// 详情页
+	var detail = $('.show-detail');
 
 // 主界面中dc的两个按钮
 	var favourBtn = $('.favour-dc');
 	var detailBtn = $('.detail-dc');
 
+// 收藏夹中的两个按钮
+	var deleteBtn = $('.favour-delete');
+	var favourDetailBtn = $('.favour-detail');
+
+// 详情页中发送评论的按钮
+	var commentBtn = $('.submit-comment');
 
 
 // 当前textarea的状态，false说明尚未展开成输入界面
@@ -211,12 +307,12 @@ $(document).ready(function(){
 
 // 输入框被点击时展开成输入界面
 	textarea.click(function(){
-		showInput(hideTxt,back);
+		showInput(hideTxt,toMain);
 	});
 
 
 // 由输入界面返回主界面
-	function back(){
+	function toMain(){
 		console.log(dcState);
 		if (dcState == true) {
 			submitBtn.addClass('hide-ele');
@@ -232,6 +328,39 @@ $(document).ready(function(){
 			dcState = false;
 		}
 	}
+
+
+
+	// 主界面跳转至详情页
+	function toDetail(){
+		// 隐藏主界面
+		dcContainer.addClass('hide-main');
+		nav.addClass('hide-nav');
+		// 显示详情页
+		detail.removeClass('hide-detail');
+		// 渲染评论
+		var username = check.dailyCss.username;
+		tpl.push({
+			username: username
+		}); 
+		drawComment(check.data);
+		tpl.pop();
+	}
+	favourDetailBtn.click(toDetail);
+
+	// 评论区中将滚动条拉至底部
+	function scroll(){
+		$('.comment-container').scrollTop($('.comment-container').height());
+	}
+
+
+	// 详情页中发送评论
+	function comment(){
+		var commentTxt = $('.show-input').val();
+		scroll();
+	}
+	commentBtn.click(comment);
+	// console.log($('.comment-container').scrollTop);
 
 });
 
