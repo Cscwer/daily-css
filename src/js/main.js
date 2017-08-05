afb4db401f0eb3afcfd8258e4d1bafb0595ac7cb
IFE <971340448@qq.com>
2017-08-05 16:58:38 +0800

    done

--- src/js/main.js
+++ src/js/main.js
@@ -1,32 +1,3 @@
-// main.js
-
-
-// function drawDC(data){
-
-	
-	
-// 	var dailyCSSList = `
-// 		{{ get (item, idx) >>>> list2 }}
-// 			<span> {{ item }} </span>
-// 		{{ teg }}
-
-// 		<ul>
-// 			{{ get (item, idx) >>>> list }}
-// 			<li> {{ item }} </li>
-// 			{{ teg }}
-// 		</ul>
-// 	`; 
-
-	
-
-// 	var DCRender = tpl.fromStr(dailyCSSList); 
-
-// 	var result = DCRender(data); 
-
-// 	$('.container').html(result);
-
-// }
-
 
 
 
